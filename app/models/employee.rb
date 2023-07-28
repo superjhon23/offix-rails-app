0aaae51e6380c557b4c76d08707976519aa246c8
Jhonatan Moreno <jhon.moreno.dev@gmail.com>
2023-07-27 23:53:24 -0600

    Added functionality with devise to stop non-registered users to access the app, database has been reset

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,9 @@
 class Employee < ApplicationRecord
+  before_create :default_avatar
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   belongs_to :department, counter_cache: true #Self join
   belongs_to :manager, class_name: "Employee", optional: true
   has_many :reports, class_name: "Employee", foreign_key: "manager_id", dependent: :destroy, inverse_of: "manager"
@@ -6,4 +11,10 @@ class Employee < ApplicationRecord
   #Polymorphic associations
   has_many :feedbacks, dependent: :destroy
   has_many :receive_feedbacks, class_name: "Feedback", as: :feedbackable, dependent: :destroy
+
+  private
+  def default_avatar
+    return if avatar.attached?
+    avatar.attach(io: File.open("app/assets/images/default_pic.jpg"), filename: "default_pic.jpg")
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_26_031329) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_28_050142) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,7 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_26_031329) do
   create_table "employees", force: :cascade do |t|
     t.string "name"
     t.string "nationality"
-    t.string "email"
+    t.string "email", default: "", null: false
     t.string "position"
     t.date "birth_date"
     t.integer "role"
@@ -61,8 +61,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_26_031329) do
     t.bigint "manager_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.index ["department_id"], name: "index_employees_on_department_id"
+    t.index ["email"], name: "index_employees_on_email", unique: true
     t.index ["manager_id"], name: "index_employees_on_manager_id"
+    t.index ["reset_password_token"], name: "index_employees_on_reset_password_token", unique: true
   end
 
   create_table "feedbacks", force: :cascade do |t|
